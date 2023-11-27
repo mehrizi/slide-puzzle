64752fc99dd14c27a3c6a0f1c01f090e04c73b11
mahdi.mehrizi <mahdi.mehrizi@gmail.com>
2023-11-27 13:17:14 +0300

    - Working model with guide - default image set

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.scss";
 import Puzzle from "./components/Puzzle";
-
+import defaultImage from './assets/mahdi-wall.jpg'
 function App() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [backgroundImage, setBackgroundImage] = useState<string | null>(null);
+  const [backgroundImage, setBackgroundImage] = useState<string | null>(defaultImage);
   const [imageSize, setImageSize] = useState<{
     width: number;
     height: number;
   } | null>(null);
+
+  const elementRef = useRef();
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -30,10 +32,24 @@ function App() {
   };
 
   return (
-    <>
-      <input type="file" accept="image/*" onChange={handleFileChange} />
+    <div className="main">
+      <div className="left-panel">
+        <h1>A reactive sliding puzzle</h1>
+        <button onClick={()=>elementRef.current.click()}>Change Image</button>
+      <input style={{display:'none'}} ref={elementRef} type="file" accept="image/*" onChange={handleFileChange} />
+      <div className="guide">
+      Move blocks on the right so the following image is set:
+      </div>
+      
+      {backgroundImage && <div className="image-holder">
+        <img src={backgroundImage} alt="no image"/></div>}
+
+      </div>
+      <div className="right-panel">
       {backgroundImage && <Puzzle image={backgroundImage} />}
-    </>
+      </div>
+
+    </div>
   );
 }
 
